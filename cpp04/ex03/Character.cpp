650cb891589abd2c9aea2262981e5862ca457af8
AMGHAR ELMAHDI <amgharmahdi1@gmail.com>
2023-09-19 05:07:52 +0100

    finished working on cpp04/ex03 Character.cpp and Character.hpp

--- cpp04/ex03/Character.cpp
+++ cpp04/ex03/Character.cpp
@@ -6,7 +6,7 @@
 /*   By: eamghar <eamghar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 04:38:33 by eamghar           #+#    #+#             */
-/*   Updated: 2023/09/19 04:50:07 by eamghar          ###   ########.fr       */
+/*   Updated: 2023/09/19 05:05:37 by eamghar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,3 +66,39 @@ Character &Character::operator=(const Character &other)
     return *this;
 };
 
+void    Character::equip( AMateria* m )
+{
+    for (size_t i = 0; i < 4; i++)
+    {
+        if(this->inventory[i] == NULL)
+        {
+            this->inventory[i] = m;
+            std::cout << "Character " << this->name << " equipped with " << m->getType() << std::endl;
+            return;
+        }
+    }
+    std::cout << "Character " << this->name << " can't equip " << m->getType() << std::endl;
+}
+
+void Character::unequip(int idx)
+{
+    if(this->inventory[idx] != NULL)
+    {
+        delete this->inventory[idx];
+        this->inventory[idx] = NULL;
+        std::cout << "Character " << this->name << " unequipped" << std::endl;
+    }
+    else
+        std::cout << "Character " << this->name << " can't be unequipped" << std::endl;
+}
+
+void Character::use(int idx, ICharacter& target)
+{
+    if(this->inventory[idx] != NULL)
+    {
+        this->inventory[idx]->use(target);
+        std::cout << "Character " << this->name << " used " << this->inventory[idx] << " on " << target.getName() << std::endl;
+    }
+    else
+        std::cout << "Character " << this->name << " can't use its Materia on " << target.getName() << std::endl;
+}
