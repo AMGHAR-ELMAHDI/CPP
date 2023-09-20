4935bda20da0e0edd3fc9354cccb92c7a2939bff
AMGHAR ELMAHDI <amgharmahdi1@gmail.com>
2023-09-20 19:25:38 +0100

    cpp04 done just checking before push

--- cpp04/ex02/main.cpp
+++ cpp04/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eamghar <eamghar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 19:48:40 by eamghar           #+#    #+#             */
-/*   Updated: 2023/09/03 21:53:47 by eamghar          ###   ########.fr       */
+/*   Updated: 2023/09/20 19:23:48 by eamghar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,23 @@
 #include "WrongCat.hpp"
 #include "Brain.hpp"
 
-int main()
+int main( void )
 {
-    const Animal *j = new (std::nothrow)Dog();
-    const Animal *i = new (std::nothrow)Cat();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+
     delete j;
     delete i;
+
+    // Dog basic;
+    // {
+    //     Dog tmp = basic;
+    // }
+
+    const Animal* animals[4] = { new Dog(), new Dog(), new Cat(), new Cat() };
+    for ( int i = 0; i < 4; i++ ) {
+        delete animals[i];
+    }
+
     return 0;
 }
