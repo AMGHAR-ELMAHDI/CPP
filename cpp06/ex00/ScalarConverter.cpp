205928e6eff92763117ffe59250c7b656263157e
AMGHAR ELMAHDI <amgharmahdi1@gmail.com>
2023-10-01 19:05:20 +0100

    cpp06

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -41,17 +41,7 @@ ScalarConverter::~ScalarConverter()
 
 void    ScalarConverter::convert(std::string &str)
 {
-	std::cout << "inside: " << str << std::endl;
-    //char 1 // int 2 // float 3 // double 4 // float or double = 5
-
-    if(str.compare("nan") == 0 || str.compare("-inff") == 0 || str.compare("+inff") == 0 || str.compare("nanf") == 0 || str.compare("+inf") == 0 || str.compare("-inf") == 0)
-    {
-        std::cout << "char: impossible"<< "Non displayable"<< std::endl;
-        std::cout << "int: impossible" << std::endl;
-        std::cout << "float: nanf" << std::endl;
-        std::cout << "double: nan" << std::endl;
-        return;
-    }
+    //char 1 // int 2 // float 3 // double 4 // float or double 5 // string 6
 
     int type = 0;
     int printable = 0;
@@ -66,14 +56,25 @@ void    ScalarConverter::convert(std::string &str)
     {
         for (size_t i = 0; i < str.length() ; i++)
         {
-            if(str[i] == '.')// check there is no second point
+            if(str[i] == '.' && str[i + 1] && str[i + 1] != '.')// check there is no second point
             {
                 type = 5;
                 break;
             }
         }
         if(type == 0)
-            type = 2;
+        {
+            size_t k;
+            for (k = 0; k < str.length(); k++)
+            {
+                if(std::isdigit(str[k]) == 0)
+                    break;
+            }
+            if(k == str.length())
+                type = 2;
+            else
+                type = 6;
+        }
         else
         {
             if(str[str.length() - 1] == 'f')
@@ -85,52 +86,104 @@ void    ScalarConverter::convert(std::string &str)
     else
         if(std::isprint(str[0]) == 1)
             printable = 1;
-    
-    std::cout << "type is: " << type << std::endl;
-	
-	//------------------Cast to diffrent values---------------//
 
-    cast
+	//------------------Cast to diffrent values---------------//
+    if(type == 1)
+        puts("TYPE == CHAR");
+    else if(type == 2)
+        puts("TYPE == INT");
+    else if(type == 3)
+        puts("TYPE == FLOAT");
+    else if(type == 4)
+        puts("TYPE == DOUBLE");
+    castToDifferentValues(str, type);
 }
 
 
 void    ScalarConverter::castToDifferentValues(std::string str, int  type)
 {
+    if(str.compare("nan") == 0 || str.compare("-inff") == 0 || str.compare("+inff") == 0 || str.compare("nanf") == 0 || str.compare("+inf") == 0 || str.compare("-inf") == 0)
+    {
+        std::cout << "char: impossible"<< "Non displayable"<< std::endl;
+        std::cout << "int: impossible" << std::endl;
+        std::cout << "float: nanf" << std::endl;
+        std::cout << "double: nan" << std::endl;
+        return;
+    }
+
+    char    out_char = 0;
+    int     out_int = 0;
+    float   out_float = 0;
+    double  out_double = 0;
+    
+    std::stringstream stream(str);
+    
+    switch (type)
+    {
+        case 1://char
+            out_char  = str[0];
+            out_int = static_cast <int>(out_char);
+            out_float = static_cast <float>(out_char);
+            out_double = static_cast <double>(out_char);
+
+            std::cout << "char: " << out_char << std::endl;
+            std::cout << "int: " << out_int << std::endl;
+            std::cout << "float: " << out_float << std::endl;
+            std::cout << "double: " << out_double << std::endl;
+            break;
+
+        case 2://int
+            if(!(stream >> out_int))
+                std::cout << "int Overflow Error" << std::endl;
+            else
+            {
+                out_int  = std::stoi(str);
+                out_char = static_cast <char>(out_int);
+                out_float = static_cast <float>(out_int);
+                out_double = static_cast <double>(out_int);
+
+                std::cout << "char: " << out_char << std::endl;
+                std::cout << "int: " << out_int << std::endl;
+                std::cout << "float: " << out_float << std::endl;
+                std::cout << "double: " << out_double << std::endl;
+            }
+            break;
+
+            case 3://float
+                // if(!(stream >> out_float))
+                //     std::cout << "float Overflow Error" << std::endl;
+                // else
+                // {
+                    out_float  = std::stof(str);
+                    out_char = static_cast <char>(out_float);
+                    out_int = static_cast <int>(out_float);
+                    out_double = static_cast <double>(out_float);
+
+                    std::cout << "char: " << out_char << std::endl;
+                    std::cout << "int: " << out_int << std::endl;
+                    std::cout << "float: " << out_float << std::endl;
+                    std::cout << "double: " << out_double << std::endl;
+                // }
+                break;
+
+            case 4://double
+                // if(!(stream >> out_double))
+                //     std::cout << "double Overflow Error" << std::endl;
+                // else
+                // {
+                    out_double  = std::stod(str);
+                    out_char = static_cast <char>(out_double);
+                    out_int = static_cast <int>(out_double);
+                    out_float = static_cast <float>(out_double);
+
+                    std::cout << "char: " << out_char << std::endl;
+                    std::cout << "int: " << out_int << std::endl;
+                    std::cout << "float: " << out_float << std::endl;
+                    std::cout << "double: " << out_double << std::endl;
+                // }
+                break;
+        default:
+            break;
+    }
 
 }
-
-
-
-
-
-
-//f ate the end is float else is double  
-//if there is no point its int
-//32 > 127
-//string ::string
-
-/*
-    case CHAR:
-        _c = _str[0];
-        _n = static_cast< int >( _c );
-        _f = static_cast< float >( _c );
-        _d = static_cast< double >( _c );
-        break;
-    case INT:
-        _n = std::stoi( _str );
-        _f = static_cast< float >( _n );
-        _d = static_cast< double >( _n );
-        _c = static_cast< char >( _n );
-        break;
-    case FLOAT:
-        _f = std::stof( _str );
-        _n = static_cast< int >( _f );
-        _d = static_cast< double >( _f );
-        _c = static_cast< char >( _f );
-        break;
-    case DOUBLE:
-        _d = std::stod( _str );
-        _n = static_cast< int >( _d );
-        _f = static_cast< float >( _d );
-        _c = static_cast< char >( _d );
-*/
