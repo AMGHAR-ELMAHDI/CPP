6a9cb3bce8e962c1ba68682f08116376d8cb139c
AMGHAR ELMAHDI <amgharmahdi1@gmail.com>
2023-10-09 18:57:14 +0100

    cpp07/ex00 done

---
+++ cpp07/ex00/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eamghar <eamghar@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/17 19:48:40 by eamghar           #+#    #+#             */
+/*   Updated: 2023/10/09 18:56:54 by eamghar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int main( void )
+{
+    int a = 2;
+    int b = 3;
+
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+    return 0;
+}
---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eamghar <eamghar@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/02 22:22:50 by eamghar           #+#    #+#             */
+/*   Updated: 2023/10/09 18:55:42 by eamghar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+#include <string>
+
+
+class whatever
+{
+    public:
+        whatever();
+        whatever(const whatever &obj);
+        whatever &operator=(const whatever &other);
+        ~whatever();
+
+    //-----------------------------------------------------------------//
+	
+};
+
+template<typename T>
+
+void	swap(T &a, T &b)
+{
+	T	t;
+
+	t = a;
+	a = b;
+	b = t;
+}
+
+template<typename T>
+
+T	min(T &a, T &b)
+{
+	if(a < b)
+		return(a);
+	return(b);
+}
+
+
+template<typename T>
+
+T	max(T &a, T &b)
+{
+	if(a > b)
+		return(a);
+	return(b);
+}
+
+#endif
